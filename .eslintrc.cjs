e3587ed12d1507f76f99f1c6f8929fa8b6ee7eee
“defu” <1694492671@qq.com>
2023-06-24 17:36:25 +0800

    一级和二级路由的配置

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,11 +1,12 @@
 /* eslint-env node */
 module.exports = {
-  root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended'
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest'
-  }
-}
+	root: true,
+	extends: ["plugin:vue/vue3-essential", "eslint:recommended"],
+	parserOptions: {
+		ecmaVersion: "latest",
+	},
+	//不再强制要求组件命名
+	rules: {
+		"vue/multi-word-component-names": 0,
+	},
+};
---
+++ src/views/Layout/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        我是主页组件
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>        
+    export default {
+        
+    }
+</script>
+
+<style  scoped>
+
+</style>
