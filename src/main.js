05b887ba5aad8b4a0f87de64f319378695361b14
“defu” <1694492671@qq.com>
2023-06-24 19:42:19 +0800

    静态资源的引入和创建

--- src/main.js
+++ src/main.js
@@ -1,14 +1,17 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue";
+import router from "./router";
 
-const app = createApp(App)
+// 引入初始化样式文件
+import "@/styles/common.scss";
 
-app.use(createPinia())
-app.use(router)
+const app = createApp(App);
 
-app.mount('#app')
+app.use(createPinia());
+app.use(router);
+
+app.mount("#app");
