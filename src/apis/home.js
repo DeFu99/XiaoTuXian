9ef16100d3422fbd2be28ef5c858f7ceda78c179
“defu” <1694492671@qq.com>
2023-06-27 16:38:52 +0800

    home轮播图的实现

--- src/apis/home.js
+++ src/apis/home.js
@@ -1,8 +1,13 @@
 import httpInstance from "@/untils/http";
 // 获取banner
-export function getBannerAPI() {
+export function getBannerAPI(params = {}) {
+	// 默认为1 商品为2
+	const { distributionSite = "1" } = params;
 	return httpInstance({
 		url: "home/banner",
+		params: {
+			distributionSite
+		},
 	});
 }
 
