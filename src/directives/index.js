1b9d241fc2b18340573acef0ded558d56b6b5f30
“defu” <1694492671@qq.com>
2023-06-27 10:28:31 +0800

    面包屑导航实现

--- src/directives/index.js
+++ src/directives/index.js
@@ -7,10 +7,10 @@ export const lazyPlugin = {
 		app.directive("img-lazy", {
 			// el：指令绑定的元素 img
 			mounted(el, binding) {
-				console.log(el, binding.value);
+				// console.log(el, binding.value);
 				// 结构赋值
 				const { stop } = useIntersectionObserver(el, ([{ isIntersecting }]) => {
-					console.log(isIntersecting);
+					// console.log(isIntersecting);
 					if (isIntersecting) {
 						// 进入视口区域
 						el.src = binding.value;
--- src/views/Layout/components/LayoutHeader.vue
+++ src/views/Layout/components/LayoutHeader.vue
@@ -9,7 +9,7 @@
 					<router-link to="/">首页</router-link>
 				</li>
 				<li class="home" v-for="item in categoryStore.categoryList" :key="item.id">
-					<RouterLink to="/">{{ item.name }}</RouterLink>
+					<RouterLink :to="`/category/${item.id}`">{{ item.name }}</RouterLink>
 				</li>
 			</ul>
 			<div class="search">
