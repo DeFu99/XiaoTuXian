07d2210fdca79e026e6702e9ea08b113e75cd28e
“defu” <1694492671@qq.com>
2023-06-28 15:16:34 +0800

    拆分业务逻辑

---
+++ src/views/Category/composables/useCategoryData.js
@@ -0,0 +1,24 @@
+// 封装商品相关业务
+import { getTopCategoryAPI } from "@/apis/category";
+import { onMounted, ref } from "vue";
+import { onBeforeRouteUpdate, useRoute } from "vue-router";
+
+export const useCategoryData = () => {
+	const categoryData = ref({});
+	// 接收路由传来的参数
+	const route = useRoute();
+	// 默认id，之后路由参数变化，id变化
+	const getCategory = async (id = route.params.id) => {
+		// 如何在setup中获取路由参数 useRoute() -> route 等价于this.$route
+		const res = await getTopCategoryAPI(id);
+		categoryData.value = res.result;
+	};
+	onMounted(() => getCategory());
+	// 路由参数变化时，重新调用获取商品的方法
+	onBeforeRouteUpdate(to => {
+		getCategory(to.params.id);
+	});
+	return {
+		categoryData,
+	};
+};
